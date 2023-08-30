b962fc47d288bb238d4d7ef0851cc380d271b449
Sala G7 <CSalaG7@uniboyaca.edu.co>
2023-08-30 18:47:28 -0500

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'courses',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'students',
+    loadComponent: () => import('./students/students.page').then( m => m.StudentsPage)
+  },
+  {
+    path: 'courses',
+    loadComponent: () => import('./courses/courses.page').then( m => m.CoursesPage)
   },
 ];
