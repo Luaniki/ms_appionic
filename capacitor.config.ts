09337c1881d29f7e0276562aecc1eb2ea820ef1e
Sala G7 <CSalaG7@uniboyaca.edu.co>
2023-08-30 18:41:11 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ms_appionic_clase',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
